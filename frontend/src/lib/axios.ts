7c28c73948652cb8078428a6596c56d79a774f9b
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-13 22:50:31 -0300

    commitDeploy

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
+  baseURL: import.meta.env.MODE === 'production' 
+    ? 'https://journey-chi-ten.vercel.app' 
+    : 'http://localhost:3333'
+});
