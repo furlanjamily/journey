3159e75d2692bf0cbf338240b55b438fde0f95a4
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-16 20:04:33 -0300

    commitEnv

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://journey-pg6s.vercel.app',
-  // Outros parâmetros, se necessário
-});
-
-export default api;
+  baseURL: 'http://localhost:3333'
+})
