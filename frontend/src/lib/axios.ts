b89add5ff10ff1768b983898d3e844e14c24b7c5
furlanjamily <102761258+furlanjamily@users.noreply.github.com>
2024-09-22 00:06:37 -0300

    commitDeployBD

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,6 @@
+// src/api.js
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
+  baseURL: 'https://backendtripon-1.onrender.com', // URL direta
+});
